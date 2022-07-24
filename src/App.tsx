4a87cbd53e500e6b2e12a77a4c7d0aafd648404d
周兴杰 <1660154581@qq.com>
2022-07-24 18:17:32 +0800

    创建 view目录

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import {
   HashRouter as Router,
   Switch,
-  Link,
   Route,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from 'components/Nav';
-import Layout from 'components/Layout';
-
-
-
-
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -33,36 +29,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在，你丫输错地址了吧！</div>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
   );
 }
 
