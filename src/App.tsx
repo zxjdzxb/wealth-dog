9d9a9f487bfc2033e6621584a92afe514e46dd75
周兴杰 <1660154581@qq.com>
2022-07-25 08:31:23 +0800

    添加背景色

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,15 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
+    <AppWrapper>
     <Router>
       <Switch>
         <Route path="/tags">
@@ -29,6 +35,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+      </AppWrapper>
   );
 }
 
