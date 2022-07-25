9510da89b0e029ca2863cc2fcb66b2fb03bb99e3
周兴杰 <1660154581@qq.com>
2022-07-25 10:58:55 +0800

    完成备注的功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import styled from 'styled-components';
 import {TagsSection} from './Money/TagsSection';
 import {CategorySection} from './Money/CategorySection';
-import {NotesSection} from './Money/NotesSection';
 import {NumberPadSection} from './Money/NumberPadSection';
+import {NoteSection} from './Money/NoteSection';
 
 const MyLayout = styled(Layout)`
   display:flex;
@@ -15,12 +15,7 @@ function Money() {
   return (
     <MyLayout>
       <TagsSection/>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
