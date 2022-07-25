3636ee331555cb31adaf5d4d7d1a47ab37ab97c5
周兴杰 <1660154581@qq.com>
2022-07-25 08:14:49 +0800

    当前路由图标自动高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -26,6 +26,9 @@ const NavWrapper = styled.nav`
           width: 24px;
           height: 24px;
         }
+        &.selected{
+          color: #42C2FF;
+        }
       }
     }
 `;
@@ -35,22 +38,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags"  activeClassName="selected">
             <Icon name="tag"/>
             标签页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money"  activeClassName="selected">
             <Icon name="money"/>
             记账页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="chart"/>
             统计页
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
