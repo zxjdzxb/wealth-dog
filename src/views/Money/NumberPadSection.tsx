66486eff58d877cb2d63f79fd508e5a3e5c9448d
周兴杰 <1660154581@qq.com>
2022-07-25 16:26:00 +0800

    重构 NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,54 +1,7 @@
-import styled from 'styled-components';
 import {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
-const Wrapper = styled.section`
-  display:flex;
-  flex-direction: column;
-  > .output{
-    background:#f8edeb;
-    font-size: 36px;
-    font-style: italic;
-    line-height: 72px;
-    text-align:right;
-    padding: 0 16px;
-    box-shadow: inset 0 -2px 2px -2px rgba(0,0,0,0.25);
-  }
-  > .pad{ 
-    > button{
-      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
-      &.ok{ height: 128px; float: right; }
-      &.zero{ width: 50%; }
-      &:nth-child(1){
-        background:#caf0f8;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #ade8f4;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background:#90e0ef;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background:#48cae4;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background:#00b4d8;
-      }
-      &:nth-child(12) {
-        background:#0077b6;
-      }
-      &:nth-child(14) {
-        background:#0096c7;
-      }
-    }
-  }
-`;
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
   const setOutput = (output: string) => {
@@ -62,40 +15,12 @@ const NumberPadSection: React.FC = () => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) {return;}
-        setOutput(output + '.');
-        break;
-      case '删除':
-        if (output.length === 1) {
-          setOutput('');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('确认');
-        break;
+    if (text === 'OK') {
+      // TODO
+      return;
+    }
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
     }
   };
   return (
---
+++ src/views/Money/NumberPadSection/Wrapper.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display:flex;
+  flex-direction: column;
+  > .output{
+    background:#f8edeb;
+    font-size: 36px;
+    font-style: italic;
+    line-height: 72px;
+    text-align:right;
+    padding: 0 16px;
+    box-shadow: inset 0 -2px 2px -2px rgba(0,0,0,0.25);
+  }
+  > .pad{ 
+    > button{
+      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
+      &.ok{ height: 128px; float: right; }
+      &.zero{ width: 50%; }
+      &:nth-child(1){
+        background:#caf0f8;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #ade8f4;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background:#90e0ef;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background:#48cae4;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background:#00b4d8;
+      }
+      &:nth-child(12) {
+        background:#0077b6;
+      }
+      &:nth-child(14) {
+        background:#0096c7;
+      }
+    }
+  }
+`;
+
+export {Wrapper};
