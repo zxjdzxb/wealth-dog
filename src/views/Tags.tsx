5abddcf606552341ed09c530daa8af9dfdeb7389
周兴杰 <1660154581@qq.com>
2022-07-26 10:19:11 +0800

    tag list 样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,58 @@
 import Layout from '../components/Layout';
 import React  from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  background:white;
+  > li{
+    //#e5e5e7
+    background:#caf0f8;
+    border-bottom: 1px solid #ade8f4;
+    line-height: 20px;
+    padding: 12px 16px 12px 16px;
+    margin-left: 0;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    
+  }
+`;
+const Button = styled.button`
+  font-size: 18px; border: none; padding: 8px 10px;
+  background: #00b4d8; border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map(tag =>
-          <li key={tag}>{tag}</li>
+          <li key={tag}>
+            <span className="oneLine">{tag}长很长长很长</span>
+            <Icon name="right"/>
+          </li>
         )}
-      </ol>
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
