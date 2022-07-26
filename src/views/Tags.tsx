5401daee01328d8697058dcb8719f1476a6a4a2a
周兴杰 <1660154581@qq.com>
2022-07-26 11:12:49 +0800

    重构：将 tag:string 重构为 tag:{id:number;name:string}

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -44,9 +44,9 @@ function Tags() {
     <Layout>
       <TagList>
         {tags.map(tag =>
-          <li key={tag}>
-            <Link to={'/tags/' + tag}>
-              <span className="oneLine">{tag}</span>
+          <li key={tag.id}>
+            <Link to={'/tags/' + tag.id}>
+              <span className="oneLine">{tag.id}</span>
               <Icon name="right"/>
             </Link>
           </li>
