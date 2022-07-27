e0b050e1e3027d19548ee3f12f1e3c52614c7c2e
周兴杰 <1660154581@qq.com>
2022-07-27 08:47:48 +0800

    更优雅的删除

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -8,37 +8,26 @@ const defaultTags = [
   {id: createId(), name: '行'},
 ];
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-  const findTagIndex = (id: number) => {
-    let result = -1;
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].id === id) {
-        result = i;
-        break;
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+      let result = -1;
+      for (let i = 0; i < tags.length; i++) {
+        if (tags[i].id === id) {
+          result = i;
+          break;
+        }
       }
-    }
-    return result;
-  };
-  const updateTag = (id: number, obj: { name: string }) => {
-    // 获取你要改的 tag 的下标
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 把 tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
-  };
-  const deleteTag = (id: number) => {
-    // 获取你要删的 tag 的下标
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 把 tagsClone 的第 index 删掉
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
-  };
-  return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
-};
+      return result;
+    };
+    const updateTag = (id: number, {name}: { name: string }) => {
+      setTags(tags.map(tag => tag.id === id ? {id, name: name} : tag));
+    };
+    const deleteTag = (id: number) => {
+      setTags(tags.filter(tag => tag.id !== id));
+    };
+    return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
+  }
+;
 
 export {useTags};
