c282fc7312afbc441f0f3a4ab450878dcf9f2621
周兴杰 <1660154581@qq.com>
2022-07-27 16:02:47 +0800

    删除 tag id 的展示

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -40,7 +40,7 @@ function Tags() {
         {tags.map(tag =>
           <li key={tag.id}>
             <Link to={'/tags/' + tag.id}>
-              <span className="oneLine">{tag.id}</span>
+              <span className="oneLine">{tag.name}</span>
               <Icon name="right"/>
             </Link>
           </li>
